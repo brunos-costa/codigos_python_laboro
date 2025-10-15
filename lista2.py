278bad31edc6a2cc1e2c6cf715323bd5784adccf
Bruno Costa <analistabrunocosta@gmail.com>
2025-10-15 20:57:46 -0300

    Trabalhando com lista

---
+++ lista2.py
@@ -0,0 +1,31 @@
+valores = [] # criando uma lista vazia
+
+for item in range(10,14):
+    valores.append(item)
+
+print(valores)
+
+# Preenchedo uma lista com dados dinâmicos
+valores2 = []
+while True:
+    num = int(input("Informe um valor númerico qualquer - zero para encerrar: "))
+
+    if num == 0:
+        break #encerra o sistema
+    else:
+        valores2.append(num)
+
+print("\nPrograma encerrado\n")
+print(valores2)
+
+while True:
+    print(valores2)
+
+    print("Deseja apagar? 1-sim 2-não")
+    resposta = int(input("Qual sua resposta? "))
+
+    if resposta == 1:
+        valores2.pop()
+    elif resposta==2:
+        break
+
