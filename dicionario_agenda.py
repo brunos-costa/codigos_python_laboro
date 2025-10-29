d64c323d57d77120f295336ceedc56577612d908
Bruno Costa <analistabrunocosta@gmail.com>
2025-10-29 20:53:33 -0300

    Finalizando dicionário

--- dicionario_agenda.py
+++ dicionario_agenda.py
@@ -4,15 +4,20 @@ while True:
     print("\nAgenda Eletrônica\n")
     print("1- Inserir dados na agenda")
     print("2- Excluir dados da agenda")
-    print("3- Sair do sistema")
+    print("3- Consultar todos os dados da agenda")
+    print("4- Sair do sistema")
     resposta = int(input("Qual sua escolha: "))
 
     if resposta == 1:
-        nome = input("Qual o nome do contato: ")
-        agenda[nome] = int(input(f"Qual o número de {nome}: "))
-        
-        print("Contato inserido com sucesso!")
-        print(agenda)
+        try:
+            nome = input("Qual o nome do contato: ")
+            agenda[nome] = int(input(f"Qual o número de {nome}: "))
+            
+            print("Contato inserido com sucesso!")
+            print(agenda)
+
+        except Exception:
+            print("Operação inválida. Contato não salvo.")
 
     elif resposta == 2:
         print(agenda)
@@ -27,8 +32,10 @@ while True:
         else:
             print(f"O contato {escolha} não existe na agenda")
         
-
-
     elif resposta == 3:
+        for chave, valor in agenda.items():
+            print(f"{chave} : {valor}")
+
+    elif resposta == 4:
         print("Sistema Encerrado")
         break
