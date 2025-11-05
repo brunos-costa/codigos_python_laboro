208c21b469aeb2888a89719a2067f48e5afcd283
Bruno Costa <analistabrunocosta@gmail.com>
2025-11-05 20:37:59 -0300

    Trabalhando com função e atualizando a agenda

--- dicionario_agenda.py
+++ dicionario_agenda.py
@@ -1,40 +1,59 @@
 agenda = dict()# inicializando um dicionário
 
-while True:
+# Função Menu
+def menu():
     print("\nAgenda Eletrônica\n")
     print("1- Inserir dados na agenda")
     print("2- Excluir dados da agenda")
     print("3- Consultar todos os dados da agenda")
     print("4- Sair do sistema")
+
+# Função inserir dados
+def inserir():
+    try:
+        nome = input("Qual o nome do contato: ")
+        agenda[nome] = int(input(f"Qual o número de {nome}: "))
+            
+        print("Contato inserido com sucesso!")
+        print(agenda)
+
+    except Exception:
+        print("Operação inválida. Contato não salvo.")
+
+
+# Função excluir dados
+def excluir(escolha):
+    # Verificando se o contato existe antes de excluir
+    if escolha in agenda:        
+        del agenda[escolha]
+        print("Dado excluído com sucesso!")
+        print(agenda)
+
+    else:
+        print(f"O contato {escolha} não existe na agenda")
+
+# Função consultar dados
+def consultar():
+    for chave, valor in agenda.items():
+        print(f"{chave} : {valor}")
+
+
+
+while True:
+    menu()
     resposta = int(input("Qual sua escolha: "))
 
     if resposta == 1:
-        try:
-            nome = input("Qual o nome do contato: ")
-            agenda[nome] = int(input(f"Qual o número de {nome}: "))
-            
-            print("Contato inserido com sucesso!")
-            print(agenda)
-
-        except Exception:
-            print("Operação inválida. Contato não salvo.")
-
+        inserir()
+        
     elif resposta == 2:
         print(agenda)
         escolha = input("Qual o nome do contato para excluir: ")
-        
-        # Verificando se o contato existe antes de excluir
-        if escolha in agenda:        
-            del agenda[escolha]
-            print("Dado excluído com sucesso!")
-            print(agenda)
 
-        else:
-            print(f"O contato {escolha} não existe na agenda")
-        
+        excluir(escolha)      
+       
     elif resposta == 3:
-        for chave, valor in agenda.items():
-            print(f"{chave} : {valor}")
+        consultar()
 
     elif resposta == 4:
         print("Sistema Encerrado")
